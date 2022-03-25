f49b5df2bd276273144da09ed7cf84e7d726f624
jym0415 <jym9504@gmail.com>
2022-03-25 15:20:19 +0900

    팝업,하단추가

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
     var px = $('.slider').width();
-    var dir = -1
+    var dir = -1;
 
 setInterval(function(){
     $(".slider ul").animate({"marginLeft" : px * dir},400,function(){
-        $(this).find('li').eq(0).appendTo($(this))
-        $(this).css({"marginLeft" : 0})
+        $(this).find('li').eq(0).appendTo($(this));
+        $(this).css({"marginLeft" : 0});
     })
-},3000)
+},3000);
+
+$("#trigger").click(function(){
+    $('.popup').removeClass('d-none');
+    $('.popbg').removeClass('d-none');
+});
+
+$(".popup button").click(function(){
+    $('.popup').addClass('d-none');
+    $('.popbg').addClass('d-none');
+});
 
 
 
 
-
-
-
-
-})
+});
